f7682e79cea0c6d732e449a581bec512ad5cd71d
Marco Costa <massiscosta@gmail.com>
2018-12-07 13:54:31 -0200

    metodos de lista

--- class/usuario.php
+++ class/usuario.php
@@ -55,8 +55,41 @@ class Usuario{
            
     }  
     }
-
+    
+    public static function getList(){
+        $sql=new Sql();
+        return $sql->select("select * from tb_usuarios order by deslogin;");
+    }
+    
+    public static function search($login){
+        $sql=new Sql();
+        return $sql->select("select *from tb_usuarios where deslogin Like :SEARCH order by deslogin",array(
+            ':SEARCH'=>"%".$login."%"
+        ));
+    } 
+    
+    public function login($login, $password){
+        $sql=new Sql();
+        $results=$sql->select("select * from tb_usuarios where deslogin = :LOGIN and dessenha = :PASSWORD", array(
+        ":LOGIN"=>$login,
+        ":PASSWORD"=>$password
+            
+    ));
+    
+    if (count($results)>0){
+        $row = $results[0];
+        
+        $this->setIdusuario($row['idusuario']);
+        $this->setDeslogin($row['deslogin']);
+        $this->setDessenha($row['dessenha']);
+        $this->setDtCadastro(new DateTime($row['dtcadastro']));
+           
+    } else {
+        throw new Exception("Login e/ou senha inválidos.");
+    } 
+    }
     public function __toString(){
+        
     return json_encode(array(
        "idusuario"=>$this->getIdusuario(),
         "deslogin"=>$this->getDeslogin(),
--- index.php
+++ index.php
@@ -6,9 +6,25 @@ require_once ("config.php");
 $usuarios = $sql->select("select *from tb_usuarios");
 echo json_encode($usuarios);
 */
-
+/* Carrag 1 usuário
 $user = new Usuario();
 $user->loadById(2);
 echo $user;
+  */
+/*Carrga uma lista de usuários
+$lista=Usuario::getList();
+echo json_encode($lista);
+*/
+
+/*Carrega uma lista de usuarios buscando pelo login
+$search=Usuario::search("jo");
+echo json_encode ($search);
+*/
+
+//Carraga um usuario usano o login e a senha
+$usuario = new Usuario();
+$usuario->login("root","98654");
+echo $usuario;
+
 
     
